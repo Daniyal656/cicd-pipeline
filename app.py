144b518debefd88cd9565cc5c07e105a80d117c7
Daniyal656 <dhkhokkher345@gmail.com>
2026-06-03 00:10:54 +0500

    Initial commit - Flask app + Dockerfile + CI/CD workflow

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__demoapp__)
+
+
+ @app.route('/')
+def home():
+    return '''
+   <h1> My CI/CD Pipeline </h1>
+   <p> Automatically deployed using GitHub Actions + Docker + AWS EC2</p?
+   '''
+
+
+ if__demoapp__ == '__main__:
+    app.run(host='0.0.0.0', port=5000)
