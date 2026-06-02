10afac7b73fa4f768941657035c80acb4c141636
Daniyal656 <dhkhokkher345@gmail.com>
2026-06-03 00:26:23 +0500

    Trigger CI/CD pipeline

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ def home():
     return '''
    <h1> My CI/CD Pipeline </h1>
    <p> Automatically deployed using GitHub Actions + Docker + AWS EC2</p?
+   <p> Pipeline triggered and working!</p>
    '''
 
 
