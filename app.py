53e861534e12e6c249a72e220158a427e41edac9
Daniyal656 <dhkhokkher345@gmail.com>
2026-06-04 23:56:24 +0500

    Fix app.py indentation

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask
 
-app = Flask(__demoapp__)
+app = Flask(__name__)
 
-
- @app.route('/')
+@app.route('/')
 def home():
     return '''
-   <h1> My CI/CD Pipeline </h1>
-   <p> Automatically deployed using GitHub Actions + Docker + AWS EC2</p?
-   <p> Pipeline triggered and working!</p>
-   '''
+    <h1> Daniyal's CI/CD Pipeline</h1>
+    <p>Automatically deployed using GitHub Actions + Docker + AWS EC2</p>
+    <p> Pipeline triggered and working!</p>
+    '''
 
-
- if__demoapp__ == '__main__:
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
